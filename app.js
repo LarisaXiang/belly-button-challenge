34f0db027e07fd684452543263760535ed80d7e8
LarisaXiang <134777272+LarisaXiang@users.noreply.github.com>
2023-10-01 12:48:52 +1030

    copy app code

---
+++ app.js
@@ -0,0 +1,112 @@
+// Initial load of the page
+function init() {
+  d3.json("https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json").then(data => {
+      // Populate the dropdown with sample IDs
+      let dropdown = d3.select("#selDataset");
+      data.names.forEach(name => {
+          dropdown.append("option").text(name).property("value", name);
+      });
+
+      // Load the first sample's data
+      updatePlot(data.names[0], data);
+      updateBubblePlot(data.names[0], data);
+      displayMetadata(data.names[0], data);
+
+      // console.log(data)
+  });
+}
+
+// Function to update the plot based on sampleID
+function updatePlot(sampleID, data) {
+  // Filter data based on sampleID
+  const sample = data.samples.filter(s => s.id === sampleID)[0];
+  // console.log(sample);
+
+  // Get the top 10 OTUs
+  const otuIds = sample.otu_ids.slice(0, 10).map(otu => `OTU ${otu}`).reverse();
+  const sampleValues = sample.sample_values.slice(0, 10).reverse();
+  const otuLabels = sample.otu_labels.slice(0, 10).reverse();
+
+  // Create the bar chart
+  const barData = [{
+      type: 'bar',
+      x: sampleValues,
+      y: otuIds,
+      text: otuLabels,
+      orientation: 'h'
+  }];
+
+  const barLayout = {
+      title: "Top 10 OTUs",
+      xaxis: { title: "Sample Values" },
+      yaxis: { title: "OTU ID" }
+  };
+
+  Plotly.newPlot('bar', barData, barLayout);
+}
+
+// Function to update the bubble chart based on sampleID
+function updateBubblePlot(sampleID, data) {
+  // Filter data based on sampleID
+  const sample = data.samples.filter(s => s.id === sampleID)[0];
+  // console.log (sample)
+
+  const otuIds = sample.otu_ids;
+  const sampleValues = sample.sample_values;
+  const otuLabels = sample.otu_labels;
+
+
+// Create the bubble chart data
+  const bubbleData = [{
+      type: 'scatter',
+      mode: 'markers',
+      x: otuIds,
+      y: sampleValues,
+      marker: {
+          size: sampleValues,
+          color: otuIds,
+      },
+      text: otuLabels,
+  }];
+
+  const bubbleLayout = {
+      xaxis: { title: "OTU ID" },
+      yaxis: { title: "Sample Values" }
+  };
+
+  Plotly.newPlot('bubble', bubbleData, bubbleLayout);
+
+  // Display metadata for the selected sample
+  displayMetadata(sampleID, data);
+}
+
+// Function to display sample metadata
+function displayMetadata(sampleID, data) {
+  // Find the metadata for the selected sample
+  const metadata = data.metadata.filter(m => m.id === parseInt(sampleID))[0];
+  // console.log(sample);
+
+  // Select the HTML element where the metadata will be displayed
+  const metadataPanel = d3.select("#sample-metadata");
+  
+
+  // Clear existing metadata
+  metadataPanel.html("");
+
+  // Display each key-value pair from the metadata
+  Object.entries(metadata).forEach(([key, value]) => {
+    metadataPanel.append("p").text(`${key}: ${value}`);
+  });
+}
+
+// Function to handle the dropdown menu value change
+function optionChanged(newSample) {
+  d3.json("https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json").then(data => {
+      updatePlot(newSample, data);
+      updateBubblePlot(newSample, data);
+  });
+}
+
+
+// Initialize the dashboard
+init();
